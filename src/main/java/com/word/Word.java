f37ef64bfd6eafc56182724c62122d42a0b05715
seongju1015 <gmrfkels1015@gmail.com>
2023-09-16 21:47:34 +0900

    Word 기본 구조 및 wordManager 기본 구조 구현

--- src/main/java/com/word/Word.java
+++ src/main/java/com/word/Word.java
@@ -1,5 +1,39 @@
 package com.word;
 
-public class Word {
+public class Word {private int level;
+    private String word;
+    private String meaning;
+
+    public Word(int level, String word, String meaning){
+
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
+
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
 
 }
--- src/main/java/com/word/WordManager.java
+++ src/main/java/com/word/WordManager.java
@@ -1,5 +1,12 @@
 package com.word;
 
 public class WordManager {
+    WordCRUD w = new WordCRUD();
+    Word word;
+    public void start(){
+        int count = 0;
+        int menu = 0;
+
+    }
 
 }
