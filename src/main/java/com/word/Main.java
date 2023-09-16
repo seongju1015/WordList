8ae86369667c506bd26a0dfb88da24f5070ce1d3
seongju1015 <gmrfkels1015@gmail.com>
2023-09-16 21:43:13 +0900

    Main 구현

--- src/main/java/com/word/Main.java
+++ src/main/java/com/word/Main.java
@@ -1,5 +1,16 @@
 package com.word;
 
 public class Main {
+    public static void main(String [] args){
 
+        Main m = new Main();
+        m.run();
+
+
+    }
+    public void run(){
+        WordManager w = new WordManager();
+        w.start();
+
+    }
 }
